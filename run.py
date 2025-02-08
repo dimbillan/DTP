4b1591ec8aacdcc3c9ce29238f176108ad884d09
dimbillan <oy807266@gmail.com>
2025-02-08 18:00:16 +0300

    docs ra

--- run.py
+++ run.py
@@ -1,5 +1,17 @@
+import requests
+import time
 from dtp import app
 from waitress import serve
 
 if __name__ == "__main__":
     serve(app, host="0.0.0.0", port=5000)
+
+    url = "https://dtp-rdxy.onrender.com"
+
+    while True:
+        response = requests.get(url)
+        if response.status_code == 200:
+            print(f"Başarıyla siteye bağlanıldı: {url}")
+        else:
+            print(f"Siteye bağlanırken hata oluştu: {response.status_code}")
+        time.sleep(60)
