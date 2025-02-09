7bbb9cc663b506fe27b2090ba438ff82b55753b5
dimbillan <oy807266@gmail.com>
2025-02-09 13:02:55 +0300

    Recursive request

--- run.py
+++ run.py
@@ -7,14 +7,8 @@ if __name__ == "__main__":
     serve(app, host="0.0.0.0", port=5000)
    #app.run(host="0.0.0.0", port=5000)
 
-"""
     url = "https://dtp-rdxy.onrender.com"
+    
     while True:
         response = requests.get(url)
-        if response.status_code == 200:
-            print(f"Başarıyla siteye bağlanıldı: {url}")
-        else:
-            print(f"Siteye bağlanırken hata oluştu: {response.status_code}")
         time.sleep(60)
-
-"""
