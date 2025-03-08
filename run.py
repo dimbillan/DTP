5c43e5e7288d74534497d8c2eaf2ce820a125987
dimbillan <oy807266@gmail.com>
2025-03-08 23:33:49 +0300

    a

--- run.py
+++ run.py
@@ -2,8 +2,8 @@ from dtp import app
 from waitress import serve
 
 if __name__ == "__main__":
-    #serve(app)
-    app.run(debug=True, host="0.0.0.0", port="5000")
+    serve(app)
+    #app.run(debug=True, host="0.0.0.0", port="5000")
 
 #1009 lines of codes in .py files. 
 #800 lines of codes in .css files.
