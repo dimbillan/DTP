895d29d64b1cccb06faf648f8d40ac2b44340998
dimbillan <oy807266@gmail.com>
2025-03-08 22:44:05 +0300

    ma

--- dtp/students/utils.py
+++ dtp/students/utils.py
@@ -2,11 +2,12 @@ import sendgrid
 import os
 from sendgrid.helpers.mail import Mail, Email, To, Content
 def send_reset_email(student):
+    token = student.get_reset_token()
     sg = sendgrid.SendGridAPIClient(api_key=os.environ.get('SENDGRID_API_KEY'))
-    from_email = Email(os.environ.get('MAIL_DEFAULT_SENDER'))  # Change to your verified sender
-    to_email = To("4.boyut2018@gmail.com")  # Change to your recipient
-    subject = "Sending with SendGrid is Fun"
-    content = Content("text/plain", "and easy to do anywhere, even with Python")
+    from_email = Email(os.environ.get('MAIL_DEFAULT_SENDER'))
+    to_email = To(f"{student.email}")
+    subject = "Şifre Sıfırlama İsteği"
+    content = Content(f"text/plain", "Lütfen bağlantıya tıklayarak şifrenizi sıfırlayın: https://www.devamsizliktakip.info.tr/reset-password/{token}")
     mail = Mail(from_email, to_email, subject, content)
 
     # Get a JSON-ready representation of the Mail object
