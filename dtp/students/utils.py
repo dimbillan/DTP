5cc7793cce678c7826d53b79232b0204cb0b3685
dimbillan <oy807266@gmail.com>
2025-03-08 22:34:28 +0300

    ma

--- dtp/students/utils.py
+++ dtp/students/utils.py
@@ -1,18 +1,18 @@
 import sendgrid
 import os
 from sendgrid.helpers.mail import Mail, Email, To, Content
+def send_reset_email(student):
+    sg = sendgrid.SendGridAPIClient(api_key=os.environ.get('SENDGRID_API_KEY'))
+    from_email = Email(os.environ.get('MAIL_DEFAULT_SENDER'))  # Change to your verified sender
+    to_email = To("4.byout2018@gmail.com")  # Change to your recipient
+    subject = "Sending with SendGrid is Fun"
+    content = Content("text/plain", "and easy to do anywhere, even with Python")
+    mail = Mail(from_email, to_email, subject, content)
 
-sg = sendgrid.SendGridAPIClient(api_key=os.environ.get('SENDGRID_API_KEY'))
-from_email = Email(os.environ.get('MAIL_DEFAULT_SENDER'))  # Change to your verified sender
-to_email = To("4.byout2018@gmail.com")  # Change to your recipient
-subject = "Sending with SendGrid is Fun"
-content = Content("text/plain", "and easy to do anywhere, even with Python")
-mail = Mail(from_email, to_email, subject, content)
-
-# Get a JSON-ready representation of the Mail object
-mail_json = mail.get()
-
-# Send an HTTP POST request to /mail/send
-response = sg.client.mail.send.post(request_body=mail_json)
-print(response.status_code)
-print(response.headers)
+    # Get a JSON-ready representation of the Mail object
+    mail_json = mail.get()
+    print(student)
+    # Send an HTTP POST request to /mail/send
+    response = sg.client.mail.send.post(request_body=mail_json)
+    print(response.status_code)
+    print(response.headers)
