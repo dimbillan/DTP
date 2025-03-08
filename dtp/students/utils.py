3329b2346e4909d001633f980143ec5e58fb48f8
dimbillan <oy807266@gmail.com>
2025-03-08 22:37:24 +0300

    ma

--- dtp/students/utils.py
+++ dtp/students/utils.py
@@ -4,7 +4,7 @@ from sendgrid.helpers.mail import Mail, Email, To, Content
 def send_reset_email(student):
     sg = sendgrid.SendGridAPIClient(api_key=os.environ.get('SENDGRID_API_KEY'))
     from_email = Email(os.environ.get('MAIL_DEFAULT_SENDER'))  # Change to your verified sender
-    to_email = To("4.byout2018@gmail.com")  # Change to your recipient
+    to_email = To("4.boyut2018@gmail.com")  # Change to your recipient
     subject = "Sending with SendGrid is Fun"
     content = Content("text/plain", "and easy to do anywhere, even with Python")
     mail = Mail(from_email, to_email, subject, content)
