dd60c94c4ff3d6bb93af160ca79ea0ff25fc7437
dimbillan <oy807266@gmail.com>
2025-03-08 22:08:54 +0300

    ma

--- dtp/students/utils.py
+++ dtp/students/utils.py
@@ -16,8 +16,7 @@ def send_reset_email(student):
     to_email = To(student.email)
     
     # HTML içeriğini oluştur
-    html_content = HtmlContent(f"Sayın {student.name},<br><br>{current_time} tarihinde hesabınız için şifre sıfırlama talebinde bulundunuz.<br>Şifrenizi sıfırlamak için aşağıdaki linke tıklayın:<br><br><a href='https://devamsizliktakip.info.tr/reset_password/{token}'>Şifremi Sıfırla</a><br><br>Bu link 30 dakika süreyle geçerlidir.<br><br>Bu e-postayı siz istemediyseniz, lütfen dikkate almayın.<br><br>İyi günler dileriz,<br>Devamsızlık Takip Sistemi")
-
+    content = Content("text/html", f"Sayın {student.name},<br><br>{current_time} tarihinde hesabınız için şifre sıfırlama talebinde bulundunuz.<br>Şifrenizi sıfırlamak için aşağıdaki linke tıklayın:<br><br><a href='https://devamsizliktakip.info.tr/reset_password/{token}'>Şifremi Sıfırla</a><br><br>Bu link 30 dakika süreyle geçerlidir.<br><br>Bu e-postayı siz istemediyseniz, lütfen dikkate almayın.<br><br>İyi günler dileriz,<br>Devamsızlık Takip Sistemi")
     try:
         # API anahtarını kontrol et
         api_key = os.environ.get('SENDGRID_API_KEY')
@@ -29,7 +28,7 @@ def send_reset_email(student):
             from_email=sender_email,
             to_emails=to_email,
             subject='Şifre Sıfırlama İsteği',
-            html_content=html_content
+            content=content
         )
         
         # Debug için mesaj içeriğini yazdır
